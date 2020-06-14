4e5aa8a840b7d6a8d383333e89528871b8ba8295
nassoro <medydullah01@gmail.com>
2020-06-15 02:36:47 +0300

    migrating table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 class CreateUsersTable extends Migration
 {
@@ -14,11 +14,26 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->increments('id');
+            $table->string('email')->unique(); //phone or email
+            $table->string('phone')->nullable(); //phone or email
+            $table->string('dmw_token')->nullable(); // U2019-003-TGA5
+            $table->string('password')->nullable();
+
+            #Personal Details
+            $table->string('name')->nullable();
+            $table->string('gender')->nullable();
+            $table->string('marital_status')->nullable();
+
+            $table->date('dob')->nullable(); //dirt of birth
+
+            $table->string('ethnicity')->nullable();
+            $table->string('tribe')->nullable();
+            $table->string('occupation')->nullable();
+            $table->string('working_hours')->nullable();
+
+            $table->string('is_active')->nullable();
+
             $table->rememberToken();
             $table->timestamps();
         });
