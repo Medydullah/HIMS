8de1d3d64e45012e4f209be2f92d114c0cb8280d
unknown <faithomary999@gmail.com>
2020-06-15 17:21:27 +0300

    seeding

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UserSeeder::class);
+        $this->call(PermissionsSeeder::class); #req
+        $this->call(PeopleSeeder::class);
+         $this->call(SystemAdminSeeder::class);
     }
+
 }
