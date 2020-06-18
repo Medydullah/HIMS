4359ab603ea602fa8e4d0c4c5b4c893b60215bc9
nassoro <medydullah01@gmail.com>
2020-06-19 02:08:57 +0300

    table migration

--- PIMS module/database/migrations/2020_06_16_234923_create__physical_addresses_table.php
+++ PIMS module/database/migrations/2020_06_16_234923_create__physical_addresses_table.php
@@ -1,10 +1,10 @@
 <?php
 
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
-class CreateAdminsTable extends Migration
+class CreatePhysicalAddressesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,8 +13,8 @@ class CreateAdminsTable extends Migration
      */
     public function up()
     {
-        Schema::create('admins', function (Blueprint $table) {
-            $table->increments('id');
+        Schema::create('PhysicalAddresses', function (Blueprint $table) {
+            $table->id();
             $table->timestamps();
         });
     }
@@ -26,6 +26,6 @@ class CreateAdminsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('admins');
+        Schema::dropIfExists('PhysicalAddresses');
     }
 }
